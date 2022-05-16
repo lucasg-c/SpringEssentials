00d50155badadfc9aa8fd95f4fac76161fdd9c27
lucasg-c <lucasogjc@gmail.com>
2022-05-16 19:10:56 -0300

    Criados o pacote 'util' e classe 'DateUtil'. Classe ApplicationStart movida para a raíz dos pacotes da aplicação.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("anime")
 public class AnimeController {
+    @Autowired
+    private DateUtil dateUtil;
     @GetMapping(path = "list")
     public List<Anime> list()
     {

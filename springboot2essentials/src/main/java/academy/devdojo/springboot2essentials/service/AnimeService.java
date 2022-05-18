bd3f4b04cf5f1eefb38fa0ecb6142b55fc1b5019
lucasg-c <lucasogjc@gmail.com>
2022-05-18 16:33:42 -0300

    Implementando o método de salvar itens na lista Anime.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -7,11 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L,"86 —Eighty Six—"), new Anime(2L,"Vivy -Fluorite Eye's Song-"));
+    private static List<Anime> animes;
+    static
+    {
+        animes = new ArrayList<>(List.of(new Anime(1L,"86 —Eighty Six—"), new Anime(2L,"Vivy -Fluorite Eye's Song-")));
+    }
+
+
     // private final AnimeRepository animeRepository;
     public List<Anime> listAll()
     {
@@ -23,5 +31,13 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found in the database."));
+    }
+
+    public Anime save(Anime anime)
+    {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+
+        return anime;
     }
 }

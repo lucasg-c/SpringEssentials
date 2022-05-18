033809796efd9f670fdb0bd02950e8340f0d9827
lucasg-c <lucasogjc@gmail.com>
2022-05-18 15:31:54 -0300

    Implementando o retorno de um item da lista pelo Id.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -3,16 +3,25 @@ package academy.devdojo.springboot2essentials.service;
 import academy.devdojo.springboot2essentials.domain.Anime;
 import academy.devdojo.springboot2essentials.repository.AnimeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
-public class AnimeService implements AnimeRepository {
+public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L,"86 —Eighty Six—"), new Anime(2L,"Vivy -Fluorite Eye's Song-"));
     // private final AnimeRepository animeRepository;
     public List<Anime> listAll()
     {
-        return List.of(new Anime(1L,"86 —Eighty Six—"), new Anime(2L,"Vivy -Fluorite Eye's Song-"));
+        return animes;
+    }
+    public Anime findById(long id)
+    {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found in the database."));
     }
 }

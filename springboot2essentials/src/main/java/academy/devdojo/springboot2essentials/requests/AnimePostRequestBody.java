9cc726136be622b8f30975ce40da918449ab0b30
lucasg-c <lucasogjc@gmail.com>
2022-05-19 18:22:45 -0300

    Validando o campo nome com Spring Boot Validation.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
@@ -2,8 +2,12 @@ package academy.devdojo.springboot2essentials.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody
 {
+    @NotEmpty(message = "Anime's name cannot be empty.")
     private String name;
 }

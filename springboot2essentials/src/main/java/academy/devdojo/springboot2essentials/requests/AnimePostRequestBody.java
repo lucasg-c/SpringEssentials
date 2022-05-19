8149fd702cd4fe0f3554e12c27603d609e1f87d5
lucasg-c <lucasogjc@gmail.com>
2022-05-19 20:29:58 -0300

    Customizando a exceção ativada por campos inválidos.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
@@ -1,9 +1,9 @@
 package academy.devdojo.springboot2essentials.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody

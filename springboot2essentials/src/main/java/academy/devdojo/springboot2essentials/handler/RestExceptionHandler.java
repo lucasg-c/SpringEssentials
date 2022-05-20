8b48bbcb0868249d1e089982c3ff515401a8a315
lucasg-c <lucasogjc@gmail.com>
2022-05-20 15:39:06 -0300

    Padronizando exceção de POSTs cujo JSON tem formatação errada.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/handler/RestExceptionHandler.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/handler/RestExceptionHandler.java
@@ -2,14 +2,19 @@ package academy.devdojo.springboot2essentials.handler;
 
 import academy.devdojo.springboot2essentials.exception.BadRequestException;
 import academy.devdojo.springboot2essentials.exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2essentials.exception.ExceptionDetails;
 import academy.devdojo.springboot2essentials.exception.ValidationExceptionDetails;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,10 +22,10 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
 {
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre)
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre)
     {
         return new ResponseEntity<>
                 (
@@ -35,25 +40,39 @@ public class RestExceptionHandler
                 );
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception)
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request)
     {
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
         String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
 
         return new ResponseEntity<>
-                (
-                        ValidationExceptionDetails.builder()
-                                .timestamp(LocalDateTime.now())
-                                .status(HttpStatus.BAD_REQUEST.value())
-                                .title("Bad Request Exception, Invalid fields.")
-                                .details("Check the field(s) error(s).")
-                                .developerMessage(exception.getClass().getName())
-                                .fields(fields)
-                                .fieldsMessage(fieldsMessage)
-                                .build(),
-                        HttpStatus.BAD_REQUEST
-                );
+        (
+            ValidationExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Bad Request Exception, Invalid fields.")
+                .details("Check the field(s) error(s).")
+                .developerMessage(exception.getClass().getName())
+                .fields(fields)
+                .fieldsMessage(fieldsMessage)
+                .build(),
+            HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity(exceptionDetails, headers, status);
     }
 }

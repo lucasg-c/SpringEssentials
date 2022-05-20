4d2ecfe13c5795dcf143d0599e5c5218550b6010
lucasg-c <lucasogjc@gmail.com>
2022-05-20 19:21:53 -0300

    Executando método POST com RestTemplate 'postForObject' e 'exchange'.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -3,8 +3,7 @@ package academy.devdojo.springboot2essentials.client;
 import academy.devdojo.springboot2essentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -15,9 +14,11 @@ public class SpringClient
 {
     public static void main(String[] args)
     {
+        //retorna objeto em um "wrapper"
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(entity);
 
+        //retorna o objeto diretamente
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(object);
 
@@ -33,5 +34,28 @@ public class SpringClient
         );
         //@formatter:on
         log.info(exchange.getBody());
+
+//        Anime aquatope = Anime.builder().name("Shiroi Suna no Aquatope").build();
+//        Anime aquatopeSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", aquatope, Anime.class);
+//        log.info("saved anime {}", aquatopeSaved);
+
+        //@formatter:off
+        Anime toman = Anime.builder().name("Tokyo Manji Revengers").build();
+        ResponseEntity<Anime> tomanSaved = new RestTemplate().exchange
+        (
+            "http://localhost:8080/animes/",
+            HttpMethod.POST,
+            new HttpEntity<>(toman, createJsonHeader()),
+            Anime.class
+        );
+        log.info("saved anime {}", tomanSaved);
+        //@formatter:on
+    }
+
+    private static HttpHeaders createJsonHeader()
+    {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }

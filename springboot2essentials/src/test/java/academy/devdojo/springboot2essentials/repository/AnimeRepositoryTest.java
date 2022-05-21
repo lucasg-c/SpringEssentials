2d145497d3bd998c73a6a7dc5533514d1c8dcbd4
lucasg-c <lucasogjc@gmail.com>
2022-05-21 16:32:35 -0300

    Codificando e executando teste de exceção do método 'save()'.

--- springboot2essentials/src/test/java/academy/devdojo/springboot2essentials/repository/AnimeRepositoryTest.java
+++ springboot2essentials/src/test/java/academy/devdojo/springboot2essentials/repository/AnimeRepositoryTest.java
@@ -8,10 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("Tests for Anime Repository")
@@ -69,8 +68,9 @@ class AnimeRepositoryTest
         String name = animeSaved.getName();
         List<Anime> animes = this.animeRepository.findByName(name);
 
-        Assertions.assertThat(animes).isNotEmpty();
-        Assertions.assertThat(animes).contains(animeSaved);
+        Assertions.assertThat(animes)
+                .isNotEmpty()
+                .contains(animeSaved);
     }
 
     @Test
@@ -80,6 +80,19 @@ class AnimeRepositoryTest
         List<Anime> animes = this.animeRepository.findByName("Not a Listed Anime");
 
         Assertions.assertThat(animes).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Save throws ConstraintInvalidationException when name is empty")
+    public void save_ThrowsConstraintInvalidationException_WhenNameIsEmpty()
+    {
+        Anime anime = new Anime();
+//        Assertions.assertThatThrownBy(() -> this.animeRepository.save(anime))
+//                .isInstanceOf(ConstraintViolationException.class);
+
+        Assertions.assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> this.animeRepository.save(anime))
+                .withMessageContaining("Anime's name cannot be empty.");
     }
 
     private Anime createAnime()

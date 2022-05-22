00d69645bdf6ce7a7b29f79b2ef691f602b2f253
lucasg-c <lucasogjc@gmail.com>
2022-05-22 15:09:54 -0300

    Verificando configurações do CSRF token.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Log4j2
@@ -15,7 +16,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests()
+        http.csrf().disable()
+                //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()

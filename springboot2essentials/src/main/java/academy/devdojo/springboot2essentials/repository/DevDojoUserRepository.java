5e3bfc74c62fa3da2e7a38279b0e1652925d23aa
lucasg-c <lucasogjc@gmail.com>
2022-05-22 18:29:35 -0300

    Removendo imports não utilizados.

--- springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/repository/DevDojoUserRepository.java
+++ springboot2essentials/src/main/java/academy/devdojo/springboot2essentials/repository/DevDojoUserRepository.java
@@ -1,10 +1,7 @@
 package academy.devdojo.springboot2essentials.repository;
 
-import academy.devdojo.springboot2essentials.domain.Anime;
 import academy.devdojo.springboot2essentials.domain.DevDojoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface DevDojoUserRepository extends JpaRepository<DevDojoUser, Long>
 {
